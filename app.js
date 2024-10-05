0bdf78b888238c2c15e5646509091ca6458d14df
NourElloumi <ellouminour53@gmail.com>
2024-10-05 21:58:19 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function () {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+
+
+
+
+$(document).ready(function ($) {
+
+    console.clear();
+
+    ScrollOut({
+        cssProps: {
+            visibleY: true,
+            viewportY: true,
+            scrollPercentY: true
+        },
+        threshold: 0.2
+    });
+
+});  
